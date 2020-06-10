cd4442275a2af921e78ad86e9b362d941ebaa67e
TBone8472 <Mavericks@Bellerophon.fios-router.home>
2020-06-09 21:53:05 -0400

    legend fix attempt

--- **Leaflet-Step-1**/static/js/logic.js
+++ **Leaflet-Step-1**/static/js/logic.js
@@ -57,26 +57,26 @@
                 layer.bindPopup("Magnitude"+feature.properties.mag+"<br>location"+feature.properties.place);
             }
         }).addTo(myMap);
-         var legend = L.control({position: 'bottomright'});
+         // The details for the legend
+            legend.onAdd = function() {
+                var div = L.DomUtil.create("div", "info legend");
+    
+                var grades = [0, 1, 2, 3, 4, 5];
+                var colors = [      
+                 "#98ee00",
+                 "#d4ee00",
+                 "#eecc00",
+                 "#ee9c00",
+                 "#ea822c",
+                 "#ea2c2c"
+                ];
+    
+             div.innerHTML +=
+             "<i style='background: " + colors[i] + "'></i> " +
+                grades[i] + (grades[i + 1] ? "–" + grades[i + 1] + "<br>" : "+");
+        }
+        return div;
+});  
+    legend.addTo(myMap);     
 
-         legend.onAdd = function (map) {
-        
-            var div = L.DomUtil.create('div', 'info legend'),
-                 grades = [0,1,2,3,4,5],
-                
-
-             // loop through our density intervals and generate a label with a colored square for each interval
-            for (var i = 0; i < grades.length; i++) {
-                 div.innerHTML +=
-                     '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-                     grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-             }
-        
-             return div;
-         };
-        
-         legend.addTo(map);    
-      })
-
-
-      
+    
